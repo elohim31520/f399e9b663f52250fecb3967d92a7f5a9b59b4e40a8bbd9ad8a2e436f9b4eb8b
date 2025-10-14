0993c66dc8534908aa8207de88f8e76e060eb2c3
lewis <elohim31520@gmail.com>
2025-10-14 09:35:37 +0800

    fix: 修正w-100%不生效

--- app/pages/Companies.vue
+++ app/pages/Companies.vue
@@ -3,7 +3,7 @@
 		<div class="text-xl font-bold">{{ $t('companies.title') }}</div>
 		<div v-for="(company, index) in companies" :key="index" class="flex-y-center justify-between">
 			<div
-				class="w-100% flex-y-center text-#434343 h-60 px-1 shadow-primary gap-5"
+				class="w-full flex-y-center text-#434343 h-[3.75rem] px-1 shadow-primary gap-5"
 				@click="handleClick(company.symbol)"
 			>
 				<CompanyIcon :symbol="company.symbol" />
