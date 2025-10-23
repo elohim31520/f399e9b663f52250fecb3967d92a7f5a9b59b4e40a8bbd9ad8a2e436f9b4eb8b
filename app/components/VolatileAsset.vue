4edc2dde862cfa7c880ffafa0b5cd26db0dd5c9a
lewis <elohim31520@gmail.com>
2025-10-23 12:38:08 +0800

    update: volitileAsset 組件資料加上路由導航至/company-metrics

--- app/components/VolatileAsset.vue
+++ app/components/VolatileAsset.vue
@@ -4,7 +4,12 @@
 			<div class="grid grid-cols-1 gap-1 px-1 mt-2">
 				<div>
 					<ul>
-						<li v-for="(stock, index) in winners" :key="index" class="flex items-center py-5 px-2 shadow-primary gap-1">
+						<li
+							v-for="(stock, index) in winners"
+							:key="index"
+							class="flex items-center py-5 px-2 shadow-primary gap-1"
+							@click="handleRoute(stock)"
+						>
 							<SvgIcon name="icon_ghost" size="2.5rem" />
 							<span class="text-gray-600">{{ stock.name }}</span>
 							<span class="text-green-600 ml-auto">{{ stock.chg }}%</span>
@@ -17,7 +22,12 @@
 		<van-tab :title="$t('volatile_asset.weak_stocks')">
 			<div class="grid grid-cols-1 gap-1 px-1 my-2">
 				<ul>
-					<li v-for="(stock, index) in losers" :key="index" class="flex items-center py-5 px-2 shadow-primary gap-1">
+					<li
+						v-for="(stock, index) in losers"
+						:key="index"
+						class="flex items-center py-5 px-2 shadow-primary gap-1"
+						@click="handleRoute(stock)"
+					>
 						<SvgIcon name="icon_ghost" size="2.5rem" />
 						<span class="text-gray-600">{{ stock.name }}</span>
 						<span class="text-red-500 ml-auto">{{ stock.chg }}%</span>
@@ -31,8 +41,16 @@
 <script lang="ts" setup>
 	import { ref } from 'vue'
 
-	const winners = ref<any[]>([])
-	const losers = ref<any[]>([])
+	interface Stock {
+		name: string
+		symbol?: string | null
+		price: string
+		chg: number
+		ychg: string
+		cap: string
+		time: string
+	}
+
 	const activeTab = ref(0)
 	const { $api } = useNuxtApp()
 
@@ -41,6 +59,17 @@
 		return res.data
 	})
 
-	winners.value = fetchedData.value?.slice(0, 5) || []
-	losers.value = fetchedData.value?.slice(-5) || []
+	const winners = computed(() => {
+		return fetchedData.value?.slice(0, 5) || []
+	})
+
+	const losers = computed(() => {
+		return fetchedData.value?.slice(-5) || []
+	})
+
+	const handleRoute = (stock: Stock) => {
+		if (stock.symbol) {
+			navigateTo(`/company-metrics/${stock.symbol}`)
+		}
+	}
 </script>
