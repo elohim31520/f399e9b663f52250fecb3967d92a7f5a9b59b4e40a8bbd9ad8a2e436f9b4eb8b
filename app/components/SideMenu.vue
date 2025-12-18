f6c91bfbb0e3fa7f69c1c9ddab41b2228cb5cd90
lewis <elohim31520@gmail.com>
2025-12-18 19:53:54 +0800

    remove: 移除about頁

--- app/components/SideMenu.vue
+++ app/components/SideMenu.vue
@@ -50,7 +50,6 @@
 
 	const menuItems = [
 		{ to: '/', icon: 'icon_settings_Name2', text: 'sidemenu.home', auth: 'always' },
-		{ to: '/about', icon: 'icon_settings_about_us', text: 'sidemenu.about', auth: 'always' },
 		{ to: '/add-transaction', icon: 'icon_menu_Opinion', text: 'sidemenu.add_record', auth: 'always' },
 		{ to: '/records', icon: 'money', text: 'sidemenu.my_records', auth: 'always' },
 		{
