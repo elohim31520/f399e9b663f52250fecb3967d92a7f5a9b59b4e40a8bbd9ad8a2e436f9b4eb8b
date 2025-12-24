e33089612761aa536561478a19b1b77ae1fb7c3f
lewis <elohim31520@gmail.com>
2025-12-24 20:44:14 +0800

    update: 修改volititle-stock組件list item上下間距

--- app/pages/volatile-stock.vue
+++ app/pages/volatile-stock.vue
@@ -1,19 +1,17 @@
 <template>
 	<div class="grid grid-cols-1 gap-1 px-1 mt-2">
-		<div>
-			<ul>
-				<li
-					v-for="(stock, index) in fetchedData"
-					:key="index"
-					class="flex items-center py-4 px-2.5 shadow-card-primary gap-5"
-					@click="handleRoute(stock)"
-				>
-					<CompanyIcon :symbol="stock.symbol || ''" />
-					<span class="text-gray-600">{{ stock.name }}</span>
-					<span class="text-green-600 ml-auto" :class="{ 'text-red-600': +stock.chg < 0 }">{{ stock.chg }}%</span>
-				</li>
-			</ul>
-		</div>
+		<ul>
+			<li
+				v-for="(stock, index) in fetchedData"
+				:key="index"
+				class="flex items-center py-1 px-2.5 shadow-card-primary gap-5"
+				@click="handleRoute(stock)"
+			>
+				<CompanyIcon :symbol="stock.symbol || ''" />
+				<span class="text-gray-600">{{ stock.name }}</span>
+				<span class="text-green-600 ml-auto" :class="{ 'text-red-600': +stock.chg < 0 }">{{ stock.chg }}%</span>
+			</li>
+		</ul>
 	</div>
 </template>
 
