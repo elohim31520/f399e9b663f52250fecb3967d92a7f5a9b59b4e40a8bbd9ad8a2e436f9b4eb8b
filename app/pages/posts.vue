b0e8de8925021e48eb237028e51f7d6336013f68
lewis <elohim31520@gmail.com>
2025-12-24 21:41:50 +0800

    update: posts頁新增看更多文章的外開連結

--- app/pages/posts.vue
+++ app/pages/posts.vue
@@ -1,5 +1,9 @@
 <template>
 	<div>
+		<a class="flex-y-center justify-center text-primary my-4" target="blank" :href="infoLink">
+			{{ $t('see_more.text') }}
+			<SvgIcon name="icon_arrow_right" size="1rem" />
+		</a>
 		<ul v-if="fetchedPost && fetchedPost.length" class="mx-4">
 			<li v-for="post in fetchedPost" :key="post.id" class="mb-20">
 				<NuxtLink :to="localePath(`/post/${post.id}`)">
@@ -15,6 +19,12 @@
 </template>
 
 <script setup lang="ts">
+	import { computed } from 'vue'
+	import { useI18n } from 'vue-i18n'
+
+	const { locale } = useI18n()
+	const localePath = useLocalePath()
+
 	interface BlogPost {
 		id: string
 		title: string
@@ -26,8 +36,10 @@
 		categories?: string[]
 		tags?: string[]
 	}
-	const { locale } = useI18n()
-	const localePath = useLocalePath()
+
+	const infoLink = computed(() => {
+		return locale.value === 'zh' ? 'https://lazypod.org/info' : 'https://lazypod.org/info-en/'
+	})
 
 	const { data: fetchedPost } = await useAsyncData<BlogPost[]>(`blog-posts-${locale.value}`, () =>
 		$fetch<BlogPost[]>('/api/blog', {
