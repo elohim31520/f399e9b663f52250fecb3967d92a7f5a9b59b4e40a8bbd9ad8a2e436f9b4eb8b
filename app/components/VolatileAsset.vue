f0a63481ae5739b4ae807e84027ce8832608931b
lewis <elohim31520@gmail.com>
2026-02-18 14:44:05 +0800

    update(ui): 修改volitileAssets組件的文字

--- app/components/VolatileAsset.vue
+++ app/components/VolatileAsset.vue
@@ -1,33 +1,37 @@
 <template>
-	<van-tabs v-model:active="activeTab" type="card" class="p-2" color="#F88379">
-		<van-tab :title="$t('volatile_asset.strong_stocks')">
-			<div class="grid grid-cols-1 gap-1 px-1 mt-2">
-				<div>
+	<div>
+		<h1 class="text-lg font-bold flex-center">{{ $t('volatile_asset.hv') }}</h1>
+		<van-tabs v-model:active="activeTab" type="card" class="p-2" color="#F88379">
+			<van-tab :title="$t('volatile_asset.strong_stocks')">
+				<div class="grid grid-cols-1 gap-1 px-1 mt-2">
+					<div>
+						<ul>
+							<li v-for="(stock, index) in winners" :key="index"
+								class="flex items-center py-5 px-2 shadow-card-primary gap-1"
+								@click="handleRoute(stock)">
+								<SvgIcon name="icon_ghost" size="2.5rem" />
+								<span class="text-gray-600">{{ stock.name }}</span>
+								<span class="text-green-600 ml-auto">{{ stock.dayChg }}%</span>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</van-tab>
+
+			<van-tab :title="$t('volatile_asset.weak_stocks')">
+				<div class="grid grid-cols-1 gap-1 px-1 my-2">
 					<ul>
-						<li v-for="(stock, index) in winners" :key="index"
+						<li v-for="(stock, index) in losers" :key="index"
 							class="flex items-center py-5 px-2 shadow-card-primary gap-1" @click="handleRoute(stock)">
 							<SvgIcon name="icon_ghost" size="2.5rem" />
 							<span class="text-gray-600">{{ stock.name }}</span>
-							<span class="text-green-600 ml-auto">{{ stock.dayChg }}%</span>
+							<span class="text-red-500 ml-auto">{{ stock.dayChg }}%</span>
 						</li>
 					</ul>
 				</div>
-			</div>
-		</van-tab>
-
-		<van-tab :title="$t('volatile_asset.weak_stocks')">
-			<div class="grid grid-cols-1 gap-1 px-1 my-2">
-				<ul>
-					<li v-for="(stock, index) in losers" :key="index"
-						class="flex items-center py-5 px-2 shadow-card-primary gap-1" @click="handleRoute(stock)">
-						<SvgIcon name="icon_ghost" size="2.5rem" />
-						<span class="text-gray-600">{{ stock.name }}</span>
-						<span class="text-red-500 ml-auto">{{ stock.dayChg }}%</span>
-					</li>
-				</ul>
-			</div>
-		</van-tab>
-	</van-tabs>
+			</van-tab>
+		</van-tabs>
+	</div>
 </template>
 
 <script lang="ts" setup>
