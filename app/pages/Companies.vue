72427b4c9c79e8829041043256f825181d7a7339
lewis <elohim31520@gmail.com>
2026-03-02 14:34:01 +0800

    update: 公司列表組件新增index-anchor功能

--- app/pages/Companies.vue
+++ app/pages/Companies.vue
@@ -1,20 +1,26 @@
 <template>
-	<div class="bg-white p-2 shadow-card-primary">
-		<div class="text-xl font-bold">{{ $t('companies.title') }}</div>
-		<div v-for="(company, index) in companies" :key="index" class="flex-y-center justify-between">
-			<div class="w-full flex-y-center text-#434343 h-[3.75rem] px-1 shadow-card-primary gap-5"
-				@click="handleClick(company.symbol)">
-				<CompanyIcon :symbol="company.symbol" />
-				<div>{{ company.symbol }}</div>
-				<div class="ml-auto">{{ company.name }}</div>
-			</div>
-		</div>
+	<div class="bg-white shadow-card-primary">
+		<div class="text-xl font-bold p-2">{{ $t('companies.title') }}</div>
+
+		<van-index-bar :index-list="indexList">
+			<template v-for="(group, letter) in groupedCompanies" :key="letter">
+				<van-index-anchor :index="letter" />
+				<div v-for="company in group" :key="company.symbol" class="flex-y-center justify-between">
+					<div class="w-full flex-y-center text-#434343 h-[3.75rem] px-1 shadow-card-primary gap-5"
+						@click="handleClick(company.symbol)">
+						<CompanyIcon :symbol="company.symbol" />
+						<div class="text-primary font-bold">{{ company.symbol }}</div>
+						<div class="ml-2 text-sm">{{ company.name }}</div>
+					</div>
+				</div>
+			</template>
+		</van-index-bar>
+
 		<van-back-top right="1rem" bottom="3.5rem" />
 	</div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
 import type { Company } from '../types/api'
 
 const localePath = useLocalePath()
@@ -27,6 +33,21 @@ defineOptions({
 const { data: companies } = await useAsyncData<Company[]>('company-symbols', async () => {
 	const res = await $api.stock.getCompanySymbols()
 	return res.data
+})
+
+const groupedCompanies = computed(() => {
+	if (!companies.value) return {}
+
+	return companies.value.reduce((acc, company) => {
+		const letter = company.symbol?.[0]?.toUpperCase() ?? '#'
+		if (!acc[letter]) acc[letter] = []
+		acc[letter]?.push(company)
+		return acc
+	}, {} as Record<string, Company[]>)
+})
+
+const indexList = computed(() => {
+	return Object.keys(groupedCompanies.value).sort()
 })
 
 const handleClick = (symbol: string) => {
