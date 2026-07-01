3d2445a5ad0a4ca8a6af82ebbe4ff14ac104f456
lewis <versus7063@gmail.com>
2026-07-01 11:39:20 +0800

    feat(types): 擴充全域 Window 介面以支援 TradingView 型別

--- app/components/TradingviewGadget.vue
+++ app/components/TradingviewGadget.vue
@@ -10,39 +10,39 @@
 </template>
 
 <script lang="ts" setup>
-	import { onMounted } from 'vue'
+import { onMounted } from 'vue'
 
-	const props = defineProps<{
-		symbol: string
-		symbolPrefix?: string
-		disabled?: boolean
-	}>()
+const props = defineProps<{
+	symbol: string
+	symbolPrefix?: string
+	disabled?: boolean
+}>()
 
-	onMounted(() => {
-		const script = document.createElement('script')
-		script.src = 'https://s3.tradingview.com/tv.js'
-		script.async = true
-		script.onload = () => {
-			new (window as any).TradingView.widget({
-				autosize: true,
-				symbol: `${props.symbolPrefix ? props.symbolPrefix : 'NASDAQ'}:${props.symbol}`,
-				interval: 'D',
-				timezone: 'Etc/UTC',
-				theme: 'light',
-				style: '1',
-				locale: 'en',
-				enable_publishing: false,
-				allow_symbol_change: true,
-				container_id: 'tradingview_12345',
-			})
-		}
-		document.body.appendChild(script)
-	})
+onMounted(() => {
+	const script = document.createElement('script')
+	script.src = 'https://s3.tradingview.com/tv.js'
+	script.async = true
+	script.onload = () => {
+		new window.TradingView.widget({
+			autosize: true,
+			symbol: `${props.symbolPrefix ? props.symbolPrefix : 'NASDAQ'}:${props.symbol}`,
+			interval: 'D',
+			timezone: 'Etc/UTC',
+			theme: 'light',
+			style: '1',
+			locale: 'en',
+			enable_publishing: false,
+			allow_symbol_change: true,
+			container_id: 'tradingview_12345',
+		})
+	}
+	document.body.appendChild(script)
+})
 </script>
 
 <style scoped>
-	.tradingview-widget-container {
-		height: 500px;
-		width: 100%;
-	}
+.tradingview-widget-container {
+	height: 500px;
+	width: 100%;
+}
 </style>
